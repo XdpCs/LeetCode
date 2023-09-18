9e0cf0fadabb75bb73c5ba394f81bf831d71a140
Alan Xu <xdpcsyy@gmail.com>
2023-09-18 21:37:25 +0800

    feat(2235, 2469): add leetcode 2235, 2469 solutions
    add leetcode 2235, 2469 solutions

---
+++ leetcode/2235:Add-Two-Integers/2235:Add-Two-Integers.go
@@ -0,0 +1,10 @@
+package leetcode
+
+// @Title        2469:Convert-the-Temperature.go
+// @Description  2235:Add-Two-Integers solution
+// @Create       XdpCs 2023-09-18 21:05
+// @Update       XdpCs 2023-09-18 21:05
+
+func sum(num1 int, num2 int) int {
+	return num1 + num2
+}
---
+++ leetcode/2469:Convert-the-Temperature/2469:Convert-the-Temperature_test.go
@@ -0,0 +1,25 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+// @Title        2469:Convert-the-Temperature_test.go
+// @Description  2469:Convert-the-Temperature solution test
+// @Create       XdpCs 2023-09-18 21:05
+// @Update       XdpCs 2023-09-18 21:05
+
+func TestSum(t *testing.T) {
+	testCases := []struct {
+		celsius float64
+	}{
+		{36.50},
+		{122.11},
+	}
+
+	fmt.Println("------------------------LeetCode Problem 2469------------------------")
+	for _, testCase := range testCases {
+		fmt.Printf("Input: %f Output: %v\n", testCase.celsius, convertTemperature(testCase.celsius))
+	}
+}
