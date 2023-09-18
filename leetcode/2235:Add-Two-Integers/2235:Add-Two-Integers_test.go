c52a1a45831d794c770e0eb9f5e450dfdec872c8
Alan Xu <xdpcsyy@gmail.com>
2023-09-18 21:38:57 +0800

    2023 sep 18 (#3)
    * feat(2235, 2469): add leetcode 2235, 2469 solutions
    
    add leetcode 2235, 2469 solutions

---
+++ leetcode/2235:Add-Two-Integers/2235:Add-Two-Integers_test.go
@@ -0,0 +1,28 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+// @Title        2235:Add-Two-Integers_test.go
+// @Description  2235:Add-Two-Integers solution test
+// @Create       XdpCs 2023-09-18 21:05
+// @Update       XdpCs 2023-09-18 21:05
+
+func TestSum(t *testing.T) {
+	testCases := []struct {
+		num1 int
+		num2 int
+	}{
+		{1118, 1114},
+		{12, 5},
+		{-10, 4},
+		{0, 0},
+	}
+
+	fmt.Println("------------------------LeetCode Problem 2235------------------------")
+	for _, testCase := range testCases {
+		fmt.Printf("Input: %d, %d Output: %d\n", testCase.num1, testCase.num2, sum(testCase.num1, testCase.num2))
+	}
+}
---
+++ leetcode/2469:Convert-the-Temperature/2469:Convert-the-Temperature.go
@@ -0,0 +1,10 @@
+package leetcode
+
+// @Title        2469:Convert-the-Temperature.go
+// @Description  2469:Convert-the-Temperature solution
+// @Create       XdpCs 2023-09-18 21:05
+// @Update       XdpCs 2023-09-18 21:05
+
+func convertTemperature(celsius float64) []float64 {
+	return []float64{celsius + 273.15, celsius*1.80 + 32.00}
+}
